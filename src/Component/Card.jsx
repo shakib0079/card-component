2f4bcdf9a3bc23336fe4065b144195cba32bda80
shakib0079 <shakib100630@gmail.com>
2024-11-24 00:45:02 +0600

    error fix

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import './Card.css';
 
 function Card({image, content, userImg, name, date}){
@@ -19,3 +20,4 @@ function Card({image, content, userImg, name, date}){
 }
 
 export default Card;
+
